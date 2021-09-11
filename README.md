4c3df0a12ceb728b4316850ed9534e452453e79d
KieranNicklas <kierannicklas@btinternet.com>
2021-09-11 11:00:22 +0000

    Introduction of functions to keep track of game and to increment scores, plus entry to README file

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-Content:
-rock, Paper, Scissors, Lizard, Spock
+# Can you Rock, Paper, Scissors, Lizard, Spock?
+
+The game, *"Can you Rock, Paper, Scissors, Lizard, Spock?"* is an extension based on the classic Rock, Paper, Scissors game with two additional options. The game design provides an opportunity for children and adults of all ages to play, with an element of chance to win each round against a randomized option from the computer. As the game is against the computer, there is a lesser chance of the round being a draw, and a greater chance of having an outright winner at the end. 
+
+## Features:
+### Existing Features:
+- Header
+    * The title of the game features dominantly at the top and center of the page, clearly displaying the game. The question mark, creates a personal feel the user as if the game is asking: *"Can you win this game"*
+
+- The Options
+    * The *Start Game* option is designed to allow the user more flexibility with what is displayed. It allows the rules to be visible when first accessing the site, but not once the game has begun.
+    * The two options stationed directly underneath the title allows the user to navigate the game. By selecting *restart*, the user will be taken to the beginning of the game, with all scores and move numbers reset.
+
+- Game Options
+    * Once the game has begun, the user is presented with five individual options which represent the elements of the game: Rock, Paper, Scissors, Lizard, Spock". The user selects one of the options, which then displays the corresponding image into the gameboard area. The computer instantly selects a random option and this in turn displays the corresponding image. The JavaScript them determines who the winner is based on the rules, and increments a point to to the winner. The one with the most points after 21 moves wins. The use of an odd number ensures that the game cannot be ended on a draw.
+
+- Additional Features
+    * As an additional feature, I would like to add more options for the user and computer to select to expand the game's variation.
+
+## Testing
+
+## Deployment
+
+## Credit
+- Images
+    * Image for the generic Rock, Paper, Scissors, Lizard, Spock found from https://www.pngegg.com/en/png-ycasl
+    * Individual images of all the playing pieces found on https://www.pngegg.com/en/png-iiamd
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,15 @@
 /* Variables to keep track of the game */
 let playerScore = 0;
 let computerScore = 0;
-let totalMoves = 20;
-let currentMove = 0;
+let totalMoves = 21;
+let currentMove = -1;
 
+const numberOfMoves = document.getElementById ('number-of-moves');
+const playersScore = document.getElementById ('player-score');
+const computersScore = document.getElementById ('computer-score');
 const userChoices = document.getElementById ('user-choices');
 const result = document.getElementById ('result');
+const resultMessage = document.getElementById ('result-message');
 const rules = document.getElementById ('rules');
 const gameboard = document.getElementById ('gameboard-container');
 const userOptions = document.getElementById ('user-options')
@@ -14,7 +18,16 @@ const paperButton = document.getElementById ('paperBtn');
 const scissorsButton = document.getElementById ('scissorsBtn');
 const lizardButton = document.getElementById ('lizardBtn');
 const spockButton = document.getElementById ('spockBtn');
+const choices = ["Rock, Paper, Scissors, Lizard, Spock"];
+const buttons = document.getElementsByTagName ('button');
 
+/* Adds an event listener for the User buttons */
+for (let button of buttons){
+    button.addEventListener('click', function() {
+        let playerChoice = this.getAttribute("data-choice");
+        startGame(playerChoice);
+    })
+}
 
 /* Function to begin game and remove rules */
 function startGame() {
@@ -22,27 +35,77 @@ function startGame() {
         gameboard.classList.add ('hide');
         userOptions.classList.add ('hide');
         result.classList.remove ('hide');
-        overallWinner();
+        winner(playerScore, computerScore);
     }
     rules.classList.add ('hide');
     userChoices.classList.remove ('hide');
+    computerScore = 0;
+    playerScore = 0;
+    currentMove++;
+    numberOfMoves.innerText = `
+    Move ${currentMove} of ${totalMoves}`;
+    playersScore.innerText = `
+    Your Score = ${playerScore}`;
+    computersScore.innerText = `
+    Computers Score = ${computerScore}`;
+
+    const computerChoiceNumber = Math.floor(Math.random()*5);
+    const computerChoice = choices[computerChoiceNumber];
 }
 
 /* Function to determine winner of each round */
-
-
-/* Function to increment User's score */
-
-/* Function increment Computer's score */
+if (playerChoice === computerChoice) {
+    let playersChoice = document.getElementById ('players-choice');
+    playersChoice.innerText = "Tie!"
+    let computersChoice = document.getElementById ('computers-choice');
+    computersChoice.innerText = "Tie!"
+    } else if (playerChoice === "Rock") {
+        if (computerChoice === "Lizard" || computerChoice === "Scissors") {
+            playerScore++;
+        } else {
+            computerScore++;
+        }
+    } else if (playerChoice === "Scissors") {
+        if (computerChoice === "Paper" || computerChoice === "Lizard") {
+            playerScore++;
+        } else {
+            computerScore++;
+        }
+    } else if (playerChoice === "Paper") {
+        if (computerChoice === "Rock" || computerChoice === "Spock") {
+            playerScore++;
+        } else {
+            computerScore++;
+        }
+    } else if (playerChoice === "Lizard") {
+        if (computerChoice === "Spock" || computerChoice === "Paper") {
+            playerScore++;
+        } else {
+            computerScore++;
+        }
+    } else if (playerChoice === "Spock") {
+        if (computerChoice === "Scissors" || computerChoice === "Rock") {
+            playerScore++;
+        } else {
+            computerScore++;
+        }
+    }
 
 /*Function to determine overall winner */
-
+function winner (playerScore, computerScore) {
+    result.classList.remove ('hide');
+    gameboard.classList.add ('hide');
+    if (playerScore < computerScore) {
+        resultMessage.innerText = "Congratulations, you win!"
+    } else if (playerScore > computerScore) {
+        resultMessage.innerText = "A loss today, but maybe victory tomorrow?"
+    }
+}
 /* Function to restart game */
 
 function restart() {
     playerScore = 0;
     computerScore = 0;
-    totalMoves = 20;
+    totalMoves = 21;
     currentMove = 0;
-    
 }
